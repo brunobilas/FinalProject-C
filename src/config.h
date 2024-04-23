8c56f129a3e42826e1de016c7429a40847953e1f
brunobilas <brunobilas@seznam.cz>
2024-04-23 16:44:03 +0100

    basics of SDL practice

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const int HEIGTH = 720;
+const int WIDTH = 1280;
+
+#endif // CONFIG_H
--- src/main.c
+++ src/main.c
@@ -2,10 +2,9 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "config.h"
 
 int main() {
-    printf("Hello, SDL!\n");
-
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
@@ -13,7 +12,7 @@ int main() {
     }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGTH, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -29,33 +28,69 @@ int main() {
         return 1;
     }
 
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+
+    SDL_Rect player = (SDL_Rect){50, 50, 50, 100};
+    player.x += 100;
+    int speed = 2;
+
+
+    SDL_Rect enemy = (SDL_Rect){50, 250, 50, 50};
+
     SDL_Event event;
     bool quit = false;
     while (!quit){
-        while (SDL_PollEvent(&event)){
+        while (SDL_PollEvent(&event)){ //loop to quit or not quit
             if (event.type == SDL_QUIT){
-                quit = true;
-            }
-            if (event.type == SDL_KEYDOWN){
-                quit = true;
-            }
-            if (event.type == SDL_MOUSEBUTTONDOWN){
                 quit = true;
             }
         }
 
-        // Set render color to blue
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        // Clear the window with the render color
+        // Set render color to xxx
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        
+        // Clear the window with the render color (clear previous frame)
         SDL_RenderClear(renderer);
 
-        
-        // Update the screen
+        if(state[SDL_SCANCODE_D]) {
+            player.x += speed;
+        }
+
+         if(state[SDL_SCANCODE_A]) {
+            player.x -= speed;
+        }
+
+        if(state[SDL_SCANCODE_S]) {
+            player.y += speed;
+        }
+
+        if(state[SDL_SCANCODE_W]) {
+            player.y -= speed;
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &player);
+
+        if(SDL_HasIntersection(&player, &enemy) == SDL_TRUE) {
+            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_RenderFillRect(renderer, &enemy);
+        }
+
+        else{
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_RenderFillRect(renderer, &enemy);
+        }
+
+
+        // Update the screen (load next frame with current content)
         SDL_RenderPresent(renderer);
+
+    // SDL_Rect
 
     }
 
-    // Clean up resources
+    // Clean up resources (shit that happens when you close the program (when the quit loop is true))
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
